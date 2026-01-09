081242af97584e5cb3e427b2e499b727b0fa8d85
Shivaji Shill <agrojitshill@gmail.com>
2026-01-09 06:40:06 -0800

    Add files via upload

---
+++ Day_01/arithmetic+variables.py
@@ -0,0 +1,14 @@
+x = 10 # Assign 10 to variable x
+y = 5 # Assign 5 to variable y
+
+# Arithmetic
+sum_xy = x + y      # Calculate the sum of x and y
+diff_xy = x - y     # Calculate the difference of x and y
+product_xy = x * y  # Calculate the product of x and y
+division_xy = x / y # Calculate the division of x and y
+
+# Print the results
+print("sum", sum_xy)              # Print the sum
+print("diff_xy", diff_xy)         # Print the differrence
+print("product_xy", product_xy)   # Print the product
+print("division_xy", division_xy) # Print the division
