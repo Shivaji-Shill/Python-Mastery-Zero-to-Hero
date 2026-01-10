7a31643b43eeb9d9ab60f8397508acb555e73d47
Shivaji Shill <agrojitshill@gmail.com>
2026-01-10 22:57:30 +0600

    Delete Lesson_01 directory

--- Lesson_01/Basic_Arithmetic_Operations.py
+++
@@ -1,5 +0,0 @@
-# Basic arithmetic operations
-print(10 + 5) # Addition:- Output: 15
-print(10 - 5) # Subtraction:- Output: 5
-print(10 * 5) # Multiplication:- Output: 50
-print(10 / 5) # Division:- Output: 2.0
--- Lesson_01/first_program.py
+++
@@ -1,4 +0,0 @@
-# This is the very fast python program and It prints text or number on the screen
-
-# Printing "Hello World!"
-print("Hello World!") # Output: Hello World
